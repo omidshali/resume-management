4e9b4d2157705dc860133e7add5c164f07d48510
OmidShali <o.shali01@gmail.com>
2022-07-13 00:51:18 +0430

    Home Page Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Home from "./components/home";
 function App() {
   return (
-    <div className="bg-slate-700 text-3xl text-center">Resume Management</div>
+    <div className="bg-slate-700 text-3xl text-center">
+      <Home />
+    </div>
   );
 }
 
---
+++ src/components/home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>Home Menu</div>;
+};
+
+export default Home;
