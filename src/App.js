af961f114432ebf55b50dd34fe98b6641cb42f57
meysam <mey3am.zarghami@gmail.com>
2022-07-13 15:28:44 +0430

    Add Work Commponent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Home from "./components/home";
+import Work from "./components/Work";
 function App() {
   return (
     <div className="bg-slate-700 text-3xl text-center">
       <Home />
+      <Work />
     </div>
   );
 }
