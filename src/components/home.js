ce2a03efd3394245f7bb6b7c4239a9bfa5acc4c3
OmidShali <o.shali01@gmail.com>
2022-07-13 13:57:33 +0430

    create new branch

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home Menu</div>;
+  return (
+    <div>
+      <h1>Home Menu</h1>
+      <p>Input Nww Code To Branch "development"</p>
+    </div>
+  );
 };
 
 export default Home;
