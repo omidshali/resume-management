dc5e8c4c45e6bc1480d599d63b921df9e77f54e3
OmidShali <o.shali01@gmail.com>
2022-07-14 20:36:03 +0430

    change logo to NavLink

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -142,10 +142,10 @@ const NavBar = () => {
               </NavLink>
             </li>
           </ul>
-          <div className="flex text-white items-center ">
+          <NavLink to="/" className="flex text-white items-center ">
             <SvgButton data="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
             <h1 className="text-[22px] ml-2">رزومه یاب</h1>
-          </div>
+          </NavLink>
         </div>
       </div>
     </div>
