088a88b12aeff8d09434b5cf6394d972c6519474
OmidShali <o.shali01@gmail.com>
2022-07-14 17:36:50 +0430

    add image to background

--- src/components/home.js
+++ src/components/home.js
@@ -2,34 +2,31 @@ import React from "react";
 import NavBar from "./NavBar";
 import SearchBox from "./UI/SearchBox";
 
-
-
-
 const Home = () => {
   return (
-    <div>
-      
-      <div className="w-full h-[100vh] bg-gradient-to-r from-[#E8CBC0] to-[#636FA4]  ">
-        <NavBar />
-        <div className="pt-[15%] z-[10]">
-          <h1 className="text-[40px] text-center font text-gray-600">
+    <div className="w-full h-[100vh] relative ">
+      <div className="w-full h-full ">
+        <div className="absolute w-full h-full bg-gradient-to-l from-slate-900 " />
+        <img
+          className="w-full h-[100vh] object-cover "
+          src={require("../img/01.jpg")}
+          alt="pic"
+        />
+        <div className="absolute top-[30%] w-full">
+          <h1 className="text-[48px] text-center text-gray-400">
             با رزومه یاب نیروی کار دلخواه خودت را پیدا کن
           </h1>
           <SearchBox />
+          <div className="flex justify-center mt-10">
+            <button className="px-[40px] py-[7px] bg-transparent border border-gray-300 text-gray-300 rounded-full hover:bg-gray-700 hover:text-white">
+              شروع
+            </button>
+            <button className="ml-5 px-[40px] py-[7px] bg-transparent border border-gray-300 text-gray-300 rounded-full hover:bg-gray-700 hover:text-white">
+              رزومه ها
+            </button>
+          </div>
         </div>
-        <div className="flex justify-center mt-10">
-          <button className="px-[40px] py-[7px] bg-transparent border border-gray-700 text-gray-700 rounded-full hover:bg-gray-700 hover:text-white">
-            شروع
-          </button>
-          <button className="mr-5 px-[40px] py-[7px] bg-transparent border border-gray-700 text-gray-700 rounded-full hover:bg-gray-700 hover:text-white">
-            رزومه ها
-          </button>
-        </div>
-        
       </div>
-      {/* <div className="z-10">
-          <img src={require("../img/01.jpg")} alt="pic" className="absolute  top-0 h-[100vh] w-[100%]" />
-        </div> */}
     </div>
   );
 };
